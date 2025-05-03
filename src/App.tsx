74b2d43a65112810f8d28856f2c97c6d6960677f
Boris Dračka <99192323+LuliBobo@users.noreply.github.com>
2025-05-03 20:15:33 +0200

    cherry-pick: Add Cookie Policy page (resolved conflict)

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,43 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from 'react';
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import PrivacyPolicy from "./components/PrivacyPolicy";
+import CookiePolicy from "./components/CookiePolicy";
+import SignUp from "./pages/SignUp";
+import { LicenseContext, LicenseLevel } from "@/lib/license-context";
+import { CookieConsent } from "./components/ui/cookie-consent"
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/privacy" element={<PrivacyPolicy />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  const [license, setLicense] = useState<LicenseLevel>('free');
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <LicenseContext.Provider value={{ license, setLicense }}>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/privacy" element={<PrivacyPolicy />} />
+              <Route path="/cookie-policy" element={<CookiePolicy />} />
+              <Route path="/signup" element={<SignUp />} />
+              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+          <CookieConsent />
+        </TooltipProvider>
+      </LicenseContext.Provider>
+    </QueryClientProvider>
+  );
+}
 
 export default App;
