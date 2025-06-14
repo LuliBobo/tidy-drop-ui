4cfcee5e183117d94b0d6b24d4242ef855f587b7
Boris Dračka <99192323+LuliBobo@users.noreply.github.com>
2025-06-14 18:09:06 +0200

    🔧 Konsolidácia kódu: Vytvorenie novej adresárovej štruktúry
    - Pridaný lib/hooks/ adresár s re-exportami do pôvodných hooks
    - Pridaný lib/utils/ adresár s re-exportami do pôvodných utils
    - Pridaný lib/contexts/ adresár s re-exportom LicenseContext
    - Vytvorený hlavný lib/index.ts pre centrálny prístup
    
    Táto zmena umožňuje postupnú migráciu kódu bez narušenia funkčnosti.

---
+++ src/lib/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Main lib index file
+ * Re-exports all consolidated modules
+ */
+
+// Re-export consolidated modules
+export * from './hooks';
+export * from './utils';
+export * from './contexts';
---
+++ src/lib/utils/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Re-export utils from the original location
+ * This allows gradual migration to new structure while maintaining compatibility
+ */
+
+// Re-export utility functions
+export * from '../../utils/removeMetadata';
